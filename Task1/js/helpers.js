cd67eb5a76845240c94ccc1a57cc64a0bd91f24c
Alexander Holovin <dvjmixxxer@gmail.com>
2016-10-17 02:57:52 +0300

    Naming fix

--- Task1/js/helpers.js
+++ Task1/js/helpers.js
@@ -1,12 +1,12 @@
 var helpers = (function () {
-    function _objectJoin(o, key, sep) {
+    function objectJoin(o, key, sep) {
         return o.map(function(el) {
             return el[key];
         }).join(sep);
     }
 
-    return {
-        objectJoin: _objectJoin
+     return {
+        objectJoin: objectJoin
     }
 
 })();
